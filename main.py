e25c7799260672f7671db31e4c2de9b157fd58dd
msuhaib01 <warheadforehead123@gmail.com>
2024-03-11 15:15:24 +0500

    added get count letters function

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ def main():
     text = get_book_text(path_to_file)
     num_words = get_num_words(text)
     print(num_words)
+    counts = get_count_letters(text)
+    print(counts)
+    
+
+def get_count_letters(text):
+    lower_case_text = text.lower()
+    return_dict = {}
+
+    for char in lower_case_text:
+        if not (char in return_dict.keys()):
+            return_dict[char] = 1
+        else:
+            return_dict[char] += 1
+    
+    return return_dict
 
 def get_num_words(text):
     words_split = text.split()
